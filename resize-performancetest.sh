e9b4f2b7739aa75b0760fb94ad6b050ad63cd58a
Stephan Kellermayr <stephankellermayr@users.noreply.github.com>
2017-03-06 20:55:47 +0100

    Update resize-performancetest.sh

--- resize-performancetest.sh
+++ resize-performancetest.sh
@@ -19,7 +19,7 @@ cmdGdalWarp=$(which gdalwarp)
 cmdConvert=$(which convert)
 [ ${cmdConvert} ] || { echo "\"convert\" nicht gefunden. Bitte mit \"apt-get install imagemagick\" installieren."; exit; }
 
-# Klassische verkleinerung mit ImageMagick
+# Klassische Verkleinerung mit ImageMagick
 function test_im() {
 	$(which convert) -quiet "${1}" -resize 320x213 -strip -quality 85 output-im.jpg
 }
@@ -30,7 +30,7 @@ function test_gdal() {
 	${cmdGdalTranslate} -of VRT -a_srs WGS84 -a_ullr -180 90 0 -90 "${1}" gdal.vrt >/dev/null 2>&1
 	# 2. Verkleinere das (vermeintliche) GeoTIFF
 	${cmdGdalWarp} -ts 320 213 -of GTiff -r cubic gdal.vrt gdal.tif >/dev/null 2>&1
-	# 3. Konvertiere das GEOTiff zurück ins JPG-Formar
+	# 3. Konvertiere das GEOTiff zurück ins JPG-Format
 	${cmdConvert} -quiet gdal.tif -strip -quality 85 output-gdal.jpg
 }
 
